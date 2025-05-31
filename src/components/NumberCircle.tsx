f9d21384b244544e8ad2d01deab92785a6fb2b69
altairinglorious <altairinglorious@proton.me>
2025-05-31 15:18:48 +0300

    style: make NumberCircle bold

--- src/components/NumberCircle.tsx
+++ src/components/NumberCircle.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 export default function NumberCircle({ children }: PropsWithChildren) {
 	return (
-		<div className="w-8 h-8 flex items-center justify-center rounded-full bg-black text-white">
+		<div className="w-8 h-8 flex items-center justify-center rounded-full bg-black text-white font-bold">
 			{children}
 		</div>
 	);
